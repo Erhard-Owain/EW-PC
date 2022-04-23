b67756d9562ab8d84b9bd120daa05dd6c63ec371
Erhard-Owain <91005717+Erhard-Owain@users.noreply.github.com>
2022-04-23 15:41:36 +0200

    Initial commit

---
+++ DXAppHeightBeispiel/Form1.vb
@@ -0,0 +1,63 @@
+﻿Imports System.ComponentModel
+Imports System.Text
+Imports DevExpress.Skins
+Imports DevExpress.LookAndFeel
+Imports DevExpress.UserSkins
+Imports DevExpress.XtraBars
+Imports DevExpress.XtraBars.Ribbon
+
+
+Public Class Form1
+    Sub New()
+        InitSkins()
+        InitializeComponent()
+
+    End Sub
+    Sub InitSkins()
+        DevExpress.Skins.SkinManager.EnableFormSkins()
+        DevExpress.UserSkins.BonusSkins.Register()
+
+    End Sub
+
+    Shared Sub SearchResult(ByVal Pfade As String, ByVal Args() As String)
+        ' Create a PDF document processor.
+        Using documentProcessor As New PdfDocumentProcessor()
+
+            ' Define search words.   '
+            Dim words() As String = {"ISBN", "ISSN"} 'geändert vom Original
+
+            ' Load a PDF document. 
+            documentProcessor.LoadDocument(Pfade)
+
+            ' Specify the search parameters.
+            Dim searchParameters As New PdfTextSearchParameters()
+            searchParameters.CaseSensitive = True
+            searchParameters.WholeWords = True
+
+            For Each word As String In words
+                ' Get the search results from the FindText method called with search text and search parameters.
+                Dim result As PdfTextSearchResults = documentProcessor.FindText(word, searchParameters)
+
+                ' If the desired text is found, create a rectangle that corresponds to the area containing the found text and fill the rectangle.
+                Do While result.Status = PdfTextSearchStatus.Found
+                    Using graphics As PdfGraphics = documentProcessor.CreateGraphics()
+                        HighlightFoundText(graphics, result, New SolidBrush(Color.FromArgb(130, 55, 155, 255)))
+                    End Using
+                    result = documentProcessor.FindText(word, searchParameters)
+                Loop
+            Next word
+
+            ' Save the modified document.
+            documentProcessor.SaveDocument("..\..\Temp.pdf")  ' geändert vom Original
+        End Using
+    End Sub
+
+    Public Shared Sub HighlightFoundText(ByVal graphics As PdfGraphics, ByVal result As PdfTextSearchResults, ByVal brush As SolidBrush)
+        For i As Integer = 0 To result.Rectangles.Count - 1
+            Dim rect As New RectangleF(New PointF(CSng(result.Rectangles(i).Left), CSng(result.Page.CropBox.Height) - CSng(result.Rectangles(i).Top)), New SizeF(CSng(result.Rectangles(i).Width), CSng(result.Rectangles(i).Height)))
+
+            graphics.FillRectangle(brush, rect)
+        Next i
+        graphics.AddToPageForeground(result.Page, 72, 72)
+    End Sub
+End Class
---
+++ DXAppHeightBeispiel/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("DXAppHeightBeispiel")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("DXAppHeightBeispiel")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5448be95-11fe-45eb-9f6a-d1ef9c30d775")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
